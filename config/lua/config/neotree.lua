16efc0189d5629e588b21d8d83e6b7dcb85057fc
Raphaël Colin <raphael.colin68@gmail.com>
2024-02-29 15:40:55 +0100

    update neotree configuration

--- config/lua/config/neotree.lua
+++ config/lua/config/neotree.lua
@@ -1,16 +1,24 @@
 require('neo-tree').setup({
   window = {
     mappings = {
-      ["o"] = {"open"},
+      ["o"] = { "open" },
       ["uc"] = { "order_by_created", nowait = false },
       ["ud"] = { "order_by_diagnostics", nowait = false },
       ["ug"] = { "order_by_git_status", nowait = false },
       ["um"] = { "order_by_modified", nowait = false },
       ["un"] = { "order_by_name", nowait = false },
       ["us"] = { "order_by_size", nowait = false },
-      ["ut"] = { "order_by_type", nowait = false }
+      ["ut"] = { "order_by_type", nowait = false },
+      ["oc"] = "none",
+      ["od"] = "none",
+      ["og"] = "none",
+      ["om"] = "none",
+      ["on"] = "none",
+      ["os"] = "none",
+      ["ot"] = "none"
+
     }
   }
 })
 
-vim.keymap.set({'n', 'i'}, '<F1>', ':Neotree toggle<CR>', {silent=true})
+vim.keymap.set({ 'n', 'i' }, '<F1>', ':Neotree toggle<CR>', { silent = true })
