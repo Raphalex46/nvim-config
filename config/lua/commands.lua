1403289f02db614d5a1f25ae02299063879b3ff1
Raphaël Colin <raphael.colin68@gmail.com>
2024-02-29 22:54:44 +0100

    modify :Config command

--- config/lua/commands.lua
+++ config/lua/commands.lua
@@ -1,3 +1,5 @@
+local utils = require('utils')
+
 -- Definition of the :Config and :Reload commands
 do
   local init_lua
@@ -9,8 +11,33 @@ do
     config_dir = xdg_config_home
   end
   init_lua = config_dir .. '/nvim/init.lua'
-  vim.api.nvim_create_user_command('Config', ':n ' .. init_lua,
-    { desc = "Open main configuration file (init.lua)" })
+  vim.api.nvim_create_user_command('Config',
+    function(opts)
+      if #opts.fargs == 0 then
+        vim.cmd(':n' .. init_lua)
+      else
+        local dir = config_dir .. '/nvim/lua'
+        for _, v in pairs(opts.fargs) do
+          dir = dir .. '/' .. v
+        end
+        local module_file_init = dir .. '/' .. 'init.lua'
+        local module_file = dir .. '.lua'
+        if utils.file_exists(module_file_init) then
+          vim.cmd(':n' .. module_file_init)
+        else
+          if utils.file_exists(module_file) or opts.bang then
+            vim.cmd(':n' .. dir .. '.lua')
+          else
+            print('file does not exist (' .. module_file .. '). Create it or call command with \'!\'')
+          end
+        end
+      end
+    end,
+    {
+      desc = "Open main configuration file (init.lua)",
+      nargs = '*',
+      bang = true,
+    })
   vim.api.nvim_create_user_command('Reload', ':so ' .. init_lua,
     { desc = "Reload the main configuration file (init.lua)" })
 end
---
+++ config/lua/utils.lua
@@ -0,0 +1,13 @@
+local function file_exists(path)
+  local f = io.open(path, 'r')
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+return {
+  file_exists = file_exists
+}
