24c4937c6f719bbffe2ff8f75dca2627904a73eb
Raphaël Colin <raphael.colin68@gmail.com>
2024-03-02 18:38:15 +0100

    add working reload function

--- config/lua/commands.lua
+++ config/lua/commands.lua
@@ -17,6 +17,20 @@ do
   vim.api.nvim_create_user_command('Confdir', ':n' .. config_dir .. '/nvim/lua', {
     desc = "Open configuration directory (lua/)"
   })
-  vim.api.nvim_create_user_command('Reload', ':so ' .. init_lua,
+  vim.api.nvim_create_user_command('Reload',
+  function()
+    local prefixes_to_reload = {'config', 'mappings', 'colorscheme', 'commands',
+    'options', 'terminal', 'utils', 'vim-plug'}
+    -- Start by uncaching all modules
+    for name, _ in pairs(package.loaded) do
+      for _, pref in ipairs(prefixes_to_reload) do
+        if name:match('^' .. pref) then
+          package.loaded[name] = nil
+          require(name)
+          print('Reloaded \'' .. name .. '\'')
+        end
+      end
+    end
+  end,
     { desc = "Reload the main configuration file (init.lua)" })
 end
