69b23db37fa0eda7d79fdf4834138a9d5d96c086
Raphaël Colin <raphael.colin68@gmail.com>
2024-03-04 19:02:52 +0100

    update find sessions bindings

--- config/lua/mappings/sessions.lua
+++ config/lua/mappings/sessions.lua
@@ -2,5 +2,4 @@ local session_manager = require('session_manager')
 
 vim.keymap.set('n', '<leader>sl', session_manager.load_last_session)
 vim.keymap.set('n', '<leader>sc', session_manager.load_current_dir_session)
-vim.keymap.set('n', '<leader>fs', session_manager.load_session)
-
+vim.keymap.set('n', '<leader>sf', session_manager.load_session)
