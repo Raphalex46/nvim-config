1373f85c2706cfb8ce93e2bdbbf15c98142deca7
Raphaël Colin <raphael.colin68@gmail.com>
2024-03-12 11:27:36 +0100

    add binding to display definition of hovered symbol in a floating window

--- config/lua/mappings/lsp.lua
+++ config/lua/mappings/lsp.lua
@@ -2,3 +2,15 @@ vim.keymap.set('n', 'K', vim.lsp.buf.hover)
 vim.keymap.set('n', '<Leader>lf', vim.lsp.buf.format)
 vim.keymap.set('n', '<Leader>la', vim.lsp.buf.code_action)
 vim.keymap.set('n', '<Leader>ln', vim.lsp.buf.rename)
+-- Make 'gp' open a floating window to see the definition of the hovered symbol
+vim.keymap.set('n', 'gp', function()
+  vim.api.nvim_open_win(0, true, {
+    relative = 'cursor',
+    width = 80,
+    height = 10,
+    bufpos = { 0, 0 },
+    border = 'double'
+  })
+  vim.cmd('normal gd')
+end
+)
