4d57184e440e5ee80054af6820a4ab86d8bf721d
Raphaël Colin <raphael.colin68@gmail.com>
2024-06-27 16:18:58 +0200

    add keybind for previewing hunks

--- config/lua/mappings/gitgutter.lua
+++ config/lua/mappings/gitgutter.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<Leader>gj', '<Plug>(GitGutterNextHunk)')
 vim.keymap.set('n', '<Leader>gk', '<Plug>(GitGutterPrevHunk)')
+vim.keymap.set('n', '<Leader>gh', '<Plug>(GitGutterPreviewHunk)')
