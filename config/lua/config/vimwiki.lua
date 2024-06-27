ef1c66f1c75a4359b96ad3a49b2b0083fa1e93a6
Raphaël Colin <raphael.colin68@gmail.com>
2024-06-27 16:20:12 +0200

    add basic vimwiki config

--- config/lua/config/vimwiki.lua
+++ config/lua/config/vimwiki.lua
@@ -1,5 +1,3 @@
--- Config for Wimwiki is kind of weird as it is very much, not configurable with lua
-local plug_home = vim.g.plug_home
+vim.g.vimwiki_valid_html_tags = "b,i,s,u,sub,sup,kbd,br,hr,div,center,strong,em,span"
 
-vim.cmd('source ' .. plug_home .. '/vimwiki/plugin/vimwiki.vim')
-vim.cmd('let g:vimwiki_global_vars.valid_html_tags = "b,i,s,u,sub,sup,kbd,br,hr,div,center,strong,em,span"')
+require('config.custom_wiki')
