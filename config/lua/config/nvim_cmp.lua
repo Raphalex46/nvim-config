6df1496233baaca2d23d44d0842afa9b90b31381
Raphaël Colin <raphael.colin@inria.fr>
2024-09-20 09:06:48 +0000

    update nvim-cmp config to show Text completion last

--- config/lua/config/nvim_cmp.lua
+++ config/lua/config/nvim_cmp.lua
@@ -7,6 +7,11 @@ cmp.setup({
       luasnip.lsp_expand(args.body)
     end
   },
+  view = {
+    docs = {
+      auto_open = true
+    }
+  },
   completion = {
     completeopt = 'menuone'
   },
@@ -14,7 +19,7 @@ cmp.setup({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-u>'] = cmp.mapping.scroll_docs(4),
     ['<Tab>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
+      behavior = cmp.ConfirmBehavior.Insert,
       select = true
     }),
     ['<C-j>'] = cmp.mapping(function(fallback)
@@ -38,9 +43,9 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'buffer' },
-    { name = 'path' }
+    { name = 'nvim_lsp', priority = 3 },
+    { name = 'luasnip', priority = 4 },
+    { name = 'buffer', priority = -10 },
+    { name = 'path', priority = 2 }
   })
 })
