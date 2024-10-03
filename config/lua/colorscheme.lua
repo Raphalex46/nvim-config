ed5f7df8203c8547916b3a8c7abef4ca18c5a816
Raphaël Colin <raphael.colin@inria.fr>
2024-10-03 06:17:37 +0000

    change colorscheme

--- config/lua/colorscheme.lua
+++ config/lua/colorscheme.lua
@@ -1,6 +1,6 @@
 -- The 'aurora' theme completion menu is ugly, somehow this fixes it :)
 vim.cmd('colorscheme fluoromachine')
-vim.cmd('colorscheme aurora')
+-- vim.cmd('colorscheme aurora')
 
 -- Fix color of CocMenuSel
 vim.cmd('hi CocMenuSel guibg=DarkMagenta')
