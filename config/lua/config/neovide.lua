1ce665c06b23e747561369fe514c6ab8a7c1f469
Raphaël Colin <raphael.colin68@gmail.com>
2025-11-02 20:49:18 +0100

    fix: update neovide transparency setting

--- config/lua/config/neovide.lua
+++ config/lua/config/neovide.lua
@@ -1,7 +1,7 @@
 if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0.05
   vim.g.neovide_cursor_animate_in_insert_mode = false
-  vim.g.neovide_transparency = 0.95
+  vim.g.neovide_opacity = 0.95
   vim.g.neovide_cursor_trail_size = 0.5
   vim.g.neovide_scroll_animation_length = 0.1
   vim.g.neovide_cursor_vfx_mode = 'pixiedust'
