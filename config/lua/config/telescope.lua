5570f5d07a550927d775e30442ed0b3293b2a122
Raphaël Colin <raphael.colin68@gmail.com>
2025-11-02 17:22:31 +0100

    fix: drop coc extension in telescope

--- config/lua/config/telescope.lua
+++ config/lua/config/telescope.lua
@@ -4,15 +4,10 @@ require('telescope').setup({
     initial_mode = 'normal'
   },
   extensions = {
-    coc = {
-      theme = 'ivy',
-      prefer_locations = true,   -- always use Telescope locations to preview definitions/declarations/implementations etc
-    },
     ['ui-select'] = {
       require('telescope.themes').get_dropdown {}
     }
   },
 })
-require('telescope').load_extension('coc')
 require('telescope').load_extension('ui-select')
 require('telescope').load_extension('file_browser')
