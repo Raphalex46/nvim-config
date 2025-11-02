4bda6fcf90ebf4856711d6e6ddf4cb99bf1e7d4b
Raphaël Colin <raphael.colin68@gmail.com>
2025-11-02 17:59:25 +0100

    fix: remove coc telescope extensions

--- config/lua/mappings/telescope.lua
+++ config/lua/mappings/telescope.lua
@@ -16,9 +16,6 @@ do
       end
     }
   end, {})
-  vim.keymap.set('n', '<Leader>sd', extensions.coc.document_symbols, {})
-  vim.keymap.set('n', '<Leader>sw', extensions.coc.workspace_symbols, {})
-  vim.keymap.set('n', '<Leader>cd', extensions.coc.diagnostics, {})
   vim.keymap.set('n', '<Leader>fb', extensions.file_browser.file_browser, {})
 
   -- Mappings for LSP stuff
