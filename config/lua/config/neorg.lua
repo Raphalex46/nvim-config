23f9f06e2a100f17445d11b383fc369510f6d6de
Raphaël Colin <raphael.colin68@gmail.com>
2025-11-07 10:55:45 +0100

    update Neorg config

--- config/lua/config/neorg.lua
+++ config/lua/config/neorg.lua
@@ -3,6 +3,23 @@ require('neorg').setup({
     ["core.defaults"] = {},
     ["core.concealer"] = {},
     ["core.dirman"] = {},
-    ["core.keybinds"] = {}
+    ["core.keybinds"] = {},
+    ["core.export"] = {},
+    ["core.export.html"] = {},
+    ["core.esupports.hop"] = {},
+    ["core.completion"] = {
+      config = {
+        engine = "nvim-cmp",
+        name = "[Neorg]"
+      }
+    }
   }
+})
+
+-- Specific spell check options for norg files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "norg",
+  callback = function(args)
+    vim.opt_local.spell = true
+  end
 })
